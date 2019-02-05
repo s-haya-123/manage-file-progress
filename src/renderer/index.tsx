cc242253fa3910ad899b79c6d0b6a9f05fdaa7af
s-haya-123 <ganbaru.wakenai@gmail.com>
2019-02-05 23:30:15 +0900

    add webpack config

---
+++ src/renderer/index.tsx
@@ -0,0 +1,36 @@
+
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+
+// interface MyComponentProps {
+//     // 必要なプロパティを記述
+// }
+
+// interface MyComponentStates {
+//     // 必要なプロパティを記述
+// }
+
+// // クラスの場合
+// class MyComponent extends React.Component<MyComponentProps, MyComponentStates> {
+//     constructor(props: MyComponentProps) {
+//         super(props)
+//         this.state = {
+//             // MyComponentStates と一致する必要あり
+//         }
+//     }
+//     public render(): React.ReactNode {
+//         return (/* 必要なコンポーネントを記述 */)
+//     }
+// }
+
+// interface MyStatelessComponentProps {
+//     // 必要なプロパティを記述
+// }
+
+// // 関数の場合
+// function MyStatelessComponent(props: MyStatelessComponent): JSX.Element {
+//     return ( /* 必要なコンポーネントを記述 */ )
+// }
+
+// // レンダリング
+// ReactDOM.render(/*コンポーネント*/, document.getElementByName('contents');
